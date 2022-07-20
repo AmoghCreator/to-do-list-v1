4b5ef6b07cfb50775e62217df5f225c0db24aa52
Amogh <amoghpreneur@gmail.com>
2022-07-20 13:59:49 +0530

    working :-)

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine' , 'ejs');
+
+app.use(bodyParser.urlencoded({etended:true}));
+
+var tasks= [];
 
 app.get('/' , (req , res) => {
-    var today = new Date();
-    if(today.getDay() == 3) {
-        res.send("it's a Tuesday!!");
-    }
-    else {
-        res.send("go work");
-    }
+    var today  = new Date();
+    var options = {weekday : "long" , day : "numeric" , month : "long"};
+    var day = today.toLocaleDateString("en-IN" , options);
+
+    res.render("list" , {kindOfDay : day , newTask : tasks , taskLen : tasks.length , taskArray : tasks});
+})
+
+app.post('/submit' , (req , res) => {
+    task = req.body.task;
+    tasks.push(task);
+    res.redirect("/")
 })
 
 app.listen(3000 , () => {
